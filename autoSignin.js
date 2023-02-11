12d40e1de0ed288e134dc7283cfced22186fc515
mrabit <yuany@mrabit.com>
2023-02-11 21:52:59 +0800

    feat: 新增签到状态判断, 优化消息推送通知

--- autoSignin.js
+++ autoSignin.js
@@ -4,6 +4,9 @@
  * @version 0.1
  */
 
+const fetch = require('node-fetch')
+const notify = require('./sendNotify')
+
 const updateAccesssTokenURL = 'https://auth.aliyundrive.com/v2/account/token'
 const signinURL = 'https://member.aliyundrive.com/v1/activity/sign_in_list'
 
@@ -19,50 +22,86 @@ else refreshTokenArray = [refreshToken]
 
 refreshTokenArray = refreshTokenArray.filter(v => v)
 
-const fetch = require('node-fetch')
-// const notify = require('./sendNotify');
+if (!refreshTokenArray.length) {
+  console.log('未配置refreshToken, 程序终止')
+  process.exit(1)
+}
+
+// 使用 refresh_token 更新 access_token
+function updateAccesssToken(queryBody) {
+  return fetch(updateAccesssTokenURL, {
+    method: 'POST',
+    body: JSON.stringify(queryBody),
+    headers: { 'Content-Type': 'application/json' }
+  })
+    .then(res => res.json())
+    .then(d => {
+      const errorMessage = ['更新 access_token 失败']
+      const { code, nick_name, refresh_token, access_token } = d
+      if (code) {
+        if (
+          code === 'RefreshTokenExpired' ||
+          code === 'InvalidParameter.RefreshToken'
+        )
+          errorMessage.push('refresh_token 已过期或无效')
+        else errorMessage.push(message)
+        return Promise.reject(errorMessage.join(','))
+      }
+      return access_token
+    })
+}
+
+//签到
+function sign_in(queryBody, access_token) {
+  return fetch(signinURL, {
+    method: 'POST',
+    body: JSON.stringify(queryBody),
+    headers: {
+      Authorization: 'Bearer ' + access_token,
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(res => res.json())
+    .then(json => {
+      if (!json.success) {
+        return Promise.reject('签到失败')
+      }
+
+      const successMessage = ['签到成功']
+
+      const { signInLogs, signInCount } = json.result
+      const signedArray = signInLogs.filter(v => v.status === 'normal') // 已签到信息组
+      const currentSignInfo = signedArray[signedArray.length - 1] // 当天签到信息
+
+      successMessage.push(`本月累计签到 ${signInCount} 天`)
+
+      //   当天签到是否有奖励
+      if (currentSignInfo.reward)
+        successMessage.push(`本次签到获得${currentSignInfo.notice}`)
+
+      return successMessage.join(',')
+    })
+    .catch(err => console.log(err))
+}
 
 !(async () => {
-  for (const elem of refreshTokenArray) {
+  const message = []
+  for await (refresh_token of refreshTokenArray) {
     const queryBody = {
       grant_type: 'refresh_token',
-      refresh_token: elem
+      refresh_token
     }
-
-    //使用 refresh_token 更新 access_token
-    fetch(updateAccesssTokenURL, {
-      method: 'POST',
-      body: JSON.stringify(queryBody),
-      headers: { 'Content-Type': 'application/json' }
-    })
-      .then(res => res.json())
-      .then(json => {
-        // console.log(json);
-
-        let access_token = json.access_token
-        console.log(access_token)
-
-        //签到
-        fetch(signinURL, {
-          method: 'POST',
-          body: JSON.stringify(queryBody),
-          headers: {
-            Authorization: 'Bearer ' + access_token,
-            'Content-Type': 'application/json'
-          }
-        })
-          .then(res => res.json())
-          .then(json => {
-            console.log(json)
-          })
-          .catch(err => console.log(err))
-      })
-      .catch(err => console.log(err))
+    try {
+      const access_token = await updateAccesssToken(queryBody)
+      const successMessage = await sign_in(queryBody, access_token)
+      console.log(successMessage)
+      console.log('\n')
+      message.push(successMessage)
+    } catch (e) {
+      console.log(e)
+      console.log('\n')
+      message.push(e)
+    }
   }
-  // await notify.sendNotifyBark(`v2free 自动签到结果`,allnotify)
+  await notify.sendNotify(`阿里云盘签到`, message.join('\n'))
 })()
-  .catch(e => {
-    console.error(`❗️  运行错误！\n${e}`)
-  })
-  .finally()
-// notify.sendNotify(`v2free 自动签到结果`,allnotify)
