536149bb9780aafb8e1568f4ce593bbdb599a494
mrabit <yuany@mrabit.com>
2023-02-11 19:41:07 +0800

    style: .prettierc 配置

--- autoSignin.js
+++ autoSignin.js
@@ -4,8 +4,8 @@
  * @version 0.1
  */
 
-const updateAccesssTokenURL = "https://auth.aliyundrive.com/v2/account/token"
-const signinURL = "https://member.aliyundrive.com/v1/activity/sign_in_list"
+const updateAccesssTokenURL = 'https://auth.aliyundrive.com/v2/account/token'
+const signinURL = 'https://member.aliyundrive.com/v1/activity/sign_in_list'
 
 const refreshToken = process.env.refreshToken || []
 let refreshTokenArray = []
@@ -19,54 +19,50 @@ else refreshTokenArray = [refreshToken]
 
 refreshTokenArray = refreshTokenArray.filter(v => v)
 
-const fetch = require("node-fetch")
+const fetch = require('node-fetch')
 // const notify = require('./sendNotify');
 
-
-!(async() => {
-    for (const elem of refreshTokenArray) {
-        
-        const queryBody = {
-            'grant_type': 'refresh_token',
-            'refresh_token': elem
-        };
-
-        //使用 refresh_token 更新 access_token
-        fetch(updateAccesssTokenURL, {
-            method: "POST",    
-            body: JSON.stringify(queryBody),
-		    headers: {'Content-Type': 'application/json'}
-        })
-        .then((res) => res.json())
-        .then((json) => {
-            // console.log(json);
-
-            let access_token = json.access_token;
-            console.log(access_token);
-            
-            
-            //签到
-            fetch(signinURL, {
-                method: "POST",
-                body: JSON.stringify(queryBody),
-                headers: {'Authorization': 'Bearer '+access_token,'Content-Type': 'application/json'}
-            })
-            .then((res) => res.json())
-            .then((json) => {
-                console.log(json);
-            })
-            .catch((err) => console.log(err))
-            
-        })
-        .catch((err) => console.log(err))
-
-
+!(async () => {
+  for (const elem of refreshTokenArray) {
+    const queryBody = {
+      grant_type: 'refresh_token',
+      refresh_token: elem
     }
-    // await notify.sendNotifyBark(`v2free 自动签到结果`,allnotify)
-    
 
-})().catch((e) => {
+    //使用 refresh_token 更新 access_token
+    fetch(updateAccesssTokenURL, {
+      method: 'POST',
+      body: JSON.stringify(queryBody),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(res => res.json())
+      .then(json => {
+        // console.log(json);
+
+        let access_token = json.access_token
+        console.log(access_token)
+
+        //签到
+        fetch(signinURL, {
+          method: 'POST',
+          body: JSON.stringify(queryBody),
+          headers: {
+            Authorization: 'Bearer ' + access_token,
+            'Content-Type': 'application/json'
+          }
+        })
+          .then(res => res.json())
+          .then(json => {
+            console.log(json)
+          })
+          .catch(err => console.log(err))
+      })
+      .catch(err => console.log(err))
+  }
+  // await notify.sendNotifyBark(`v2free 自动签到结果`,allnotify)
+})()
+  .catch(e => {
     console.error(`❗️  运行错误！\n${e}`)
-}).finally()
+  })
+  .finally()
 // notify.sendNotify(`v2free 自动签到结果`,allnotify)
-
